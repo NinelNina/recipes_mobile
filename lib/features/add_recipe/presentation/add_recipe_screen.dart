87921c1d3d3b5508f57e7c22b6101777b357d164
NinelNina <ninkr@list.ru>
2024-04-29 19:13:29 +0300

    Changed ingredients and steps. Fixed checkbox

--- lib/features/add_recipe/presentation/add_recipe_screen.dart
+++ lib/features/add_recipe/presentation/add_recipe_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:recipes/features/common/widgets/back_icon_widget.dart';
 
@@ -10,6 +11,7 @@ class AddRecipe extends StatefulWidget {
 
 class _AddRecipeState extends State<AddRecipe> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool _isChecked = false;
   List<String> ingredients = ['ЛУК', 'ЧЕСНОК', 'ПЕРЕЦ'];
   List<String> measurements = ['шт', 'г', 'кг', 'л', 'мл'];
   List<String> categories = ['ЗАВТРАК', 'ОБЕД', 'УЖИН'];
@@ -18,20 +20,26 @@ class _AddRecipeState extends State<AddRecipe> {
 
   @override
   Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
-    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery
+        .of(context)
+        .size
+        .width;
+    double screenHeight = MediaQuery
+        .of(context)
+        .size
+        .height;
 
     return MaterialApp(
         home: Scaffold(
             backgroundColor: Colors.white,
-            body: SingleChildScrollView(
+            body: SafeArea(
               child: Column(children: [
                 NavBar(
                     height: screenHeight,
                     width: screenWidth,
                     title: "Create recipe",
                     navWidget: BackIconWidget(width: screenWidth)),
-                SingleChildScrollView(
+                Expanded(child: SingleChildScrollView(
                   padding: EdgeInsets.only(top: screenHeight * 0.023),
                   child: Column(
                     children: [
@@ -40,33 +48,32 @@ class _AddRecipeState extends State<AddRecipe> {
                       buildTitleText('RECIPE NAME'),
                       SizedBox(height: screenHeight * 0.011),
                       buildTextField(
-                          screenWidth * 0.83, screenHeight * 0.056, ''),
+                          screenWidth * 0.83, screenHeight * 0.06, ''),
                       SizedBox(height: screenHeight * 0.011),
                       buildTitleText('DISH CATEGORY'),
                       SizedBox(height: screenHeight * 0.011),
                       buildDropdown(
-                          screenWidth * 0.83, screenHeight * 0.056, categories),
+                          screenWidth * 0.83, screenHeight * 0.06, categories),
                       SizedBox(height: screenHeight * 0.011),
                       buildTitleText('DESCRIPTION'),
                       SizedBox(height: screenHeight * 0.011),
                       buildTextField(
-                          screenWidth * 0.83, screenHeight * 0.056, ''),
+                          screenWidth * 0.83, screenHeight * 0.06, ''),
                       SizedBox(height: screenHeight * 0.011),
                       buildTitleText('COOKING TIME'),
                       SizedBox(height: screenHeight * 0.011),
                       buildTextField(
-                          screenWidth * 0.83, screenHeight * 0.056, ''),
+                          screenWidth * 0.83, screenHeight * 0.06, ''),
                       SizedBox(height: screenHeight * 0.011),
                       buildTitleText('NUMBER OF SERVINGS'),
                       SizedBox(height: screenHeight * 0.011),
                       buildTextField(
-                          screenWidth * 0.83, screenHeight * 0.056, ''),
+                          screenWidth * 0.83, screenHeight * 0.06, ''),
                       SizedBox(height: screenHeight * 0.011),
                       buildTitleText('INGREDIENTS'),
                       SizedBox(height: screenHeight * 0.011),
                       Column(
                         children: [
-                          buildIngredientRow(screenWidth, screenHeight),
                           Column(children: ingredientsList)
                         ],
                       ),
@@ -77,7 +84,6 @@ class _AddRecipeState extends State<AddRecipe> {
                       SizedBox(height: screenHeight * 0.011),
                       Column(
                         children: [
-                          buildStepTextField(screenWidth, screenHeight),
                           Column(children: steps)
                         ],
                       ),
@@ -89,13 +95,14 @@ class _AddRecipeState extends State<AddRecipe> {
                         width: screenWidth * 0.9,
                         //height: screenHeight * 0.08,
                         child:
-                            buildConfirmationWidget(screenWidth, screenHeight),
+                        buildConfirmationWidget(screenWidth, screenHeight),
                       ),
                       SizedBox(height: screenHeight * 0.011),
                       buildButtonsRow(screenWidth * 0.32, screenHeight * 0.047),
                       SizedBox(height: screenHeight * 0.034)
                     ],
                   ),
+                )
                 )
               ]),
             )));
@@ -218,22 +225,49 @@ class _AddRecipeState extends State<AddRecipe> {
     );
   }
 
+  Widget buildRemoveIngredientButton(double screenWidth, double screenHeight) {
+    return SizedBox(
+      width: screenWidth * 0.05,
+      height: screenHeight * 0.026,
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            ingredientsList.removeLast();
+          });
+        },
+        child: const Icon(
+          Icons.remove,
+          color: Color(0xFFFF6E41),
+        ),
+      ),
+    );
+  }
+
+
   Widget buildIngredientRow(double screenWidth, double screenHeight) {
+    bool isRemovable = ingredientsList.isNotEmpty;
     return Column(children: [
       SizedBox(
-          width: screenWidth * 0.83,
-          height: screenHeight * 0.056,
-          child: Row(
-            children: [
-              buildDropdown(
-                  screenWidth * 0.43, screenHeight * 0.06, ingredients),
-              SizedBox(width: screenWidth * 0.007),
-              buildTextField(screenWidth * 0.18, screenHeight * 0.06, ''),
-              SizedBox(width: screenWidth * 0.007),
-              buildDropdown(
-                  screenWidth * 0.2, screenHeight * 0.04, measurements),
-            ],
-          )),
+        width: screenWidth * 0.9,
+        height: screenHeight * 0.06,
+        child: Row(
+          children: [
+            buildDropdown(screenWidth * 0.43, screenHeight * 0.06, ingredients),
+            SizedBox(width: screenWidth * 0.007),
+            buildTextField(screenWidth * 0.18, screenHeight * 0.06, ''),
+            SizedBox(width: screenWidth * 0.007),
+            buildDropdown(
+                screenWidth * 0.19, screenHeight * 0.06, measurements),
+            if (isRemovable)
+              Row(
+                children: [
+                  SizedBox(width: screenWidth * 0.024),
+                  buildRemoveIngredientButton(screenWidth, screenHeight),
+                ],
+              ),
+          ],
+        ),
+      ),
       SizedBox(height: screenHeight * 0.004)
     ]);
   }
@@ -267,66 +301,103 @@ class _AddRecipeState extends State<AddRecipe> {
     );
   }
 
-  Widget buildStepTextField(width, height) {
-    return Column( children: [SizedBox(
-        width: width * 0.83,
-        height: height * 0.056,
-        child: TextFormField(
-          decoration: InputDecoration(
-            contentPadding: const EdgeInsets.all(15),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
+  Widget buildRemoveStepButton(double screenWidth, double screenHeight) {
+    return SizedBox(
+      width: screenWidth * 0.53,
+      height: screenHeight * 0.047,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: const Color(0xFFFF6E41),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
           ),
-        )),
-      SizedBox(height: height * 0.011)
-    ]
+        ),
+        onPressed: () {
+          setState(() {
+            steps.removeLast();
+          });
+        },
+        child: const Icon(
+          Icons.remove,
+          color: Color(0xFFFF6E41),
+        ),
+      ),
     );
+  }
+
+  Widget buildStepTextField(width, height) {
+    var isRemovable = steps.length > 0;
+    return Column(children: [
+        SizedBox(
+        width: width * 0.9,
+        height: height * 0.06,
+        child: Row(children: [SizedBox(
+            width: width * 0.8,
+            height: height * 0.06,
+            child: TextFormField(
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.all(15),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            )),
+          if (isRemovable)
+            Row(
+              children: [
+                SizedBox(width: width * 0.024),
+                buildRemoveIngredientButton(width, height),
+              ],
+            ),
+        ])),
+        SizedBox(height: height * 0.011)
+        ]);
   }
 
   Widget buildConfirmationWidget(double screenWidth, double screenHeight) {
     return Row(
       children: [
         Checkbox(
-          value: false,
+          value: _isChecked, // Use the boolean variable to set the checkbox value
           checkColor: Colors.white,
           activeColor: const Color(0xFFB3261E),
-          onChanged: (_) {},
+          onChanged: (bool? newValue) {
+            setState(() {
+              _isChecked = newValue!; // Update the boolean variable when the checkbox state changes
+            });
+          },
         ),
-        /*SizedBox(
-            width: screenWidth * 0.77,
-            height: screenHeight * 0.024,
-            child: */
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: screenHeight * 0.03,
-                  child: const Text(
-                    'Post the recipe',
-                    style: TextStyle(
-                      fontFamily: 'Montserrat',
-                      fontWeight: FontWeight.w600,
-                      fontSize: 18,
-                      color: Colors.black,
-                    ),
-                  ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: screenHeight * 0.03,
+              child: const Text(
+                'Post the recipe',
+                style: TextStyle(
+                  fontFamily: 'Montserrat',
+                  fontWeight: FontWeight.w600,
+                  fontSize: 18,
+                  color: Colors.black,
                 ),
-                SizedBox(
-                    width: screenWidth * 0.77,
-                    height: screenHeight * 0.04,
-                    child: const Text(
-                      'Your recipe will be available to all users after moderation',
-                      style: TextStyle(
-                        fontFamily: 'Montserrat',
-                        fontWeight: FontWeight.w500,
-                        fontSize: 12,
-                        color: Colors.black,
-                      ),
-                    )),
-                SizedBox(height: screenHeight * 0.017)
-              ],
-            )//)
+              ),
+            ),
+            SizedBox(
+              width: screenWidth * 0.77,
+              height: screenHeight * 0.04,
+              child: const Text(
+                'Your recipe will be available to all users after moderation',
+                style: TextStyle(
+                  fontFamily: 'Montserrat',
+                  fontWeight: FontWeight.w500,
+                  fontSize: 12,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            SizedBox(height: screenHeight * 0.017)
+          ],
+        )
       ],
     );
   }
