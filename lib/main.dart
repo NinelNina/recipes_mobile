220443e3a424c806246a0e64e69cbdc9dcb5c5f2
NinelNina <ninkr@list.ru>
2024-06-20 18:48:26 +0300

    Set portrait orientation.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:recipes/features/start/presentation/start_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  //runApp(All_recipes());
-  //runApp(MyRecipes());
-  //runApp(ApplicationsForApproval());
-  //runApp(Profile(profile: "user@gmail.com",));
-  //runApp(AdminProfile(profile: "user@gmail.com",));
-  //runApp(MainPage());
-  //runApp(StatFell());
-  //runApp(Recipe_ingredients());
-  runApp(const Start());
-
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then((_) {
+    runApp(const Start());
+  });
 }
 
