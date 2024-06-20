bf0f0d853e648a64a12b4cfd386b072fb134810e
NinelNina <ninkr@list.ru>
2024-06-20 13:35:40 +0300

    Fixed safe area.

--- lib/features/sing_in/presentation/sign_in_screen.dart
+++ lib/features/sing_in/presentation/sign_in_screen.dart
@@ -74,141 +74,144 @@ class _SignInState extends State<SignIn> {
     final double width = size.width;
     final double height = size.height;
 
-    return SafeArea(
-        child: BlocProvider(
-      create: (context) =>
-          AuthenticationBloc(authenticationService: AuthenticationService()),
-      child: Scaffold(
+    return Scaffold(
         backgroundColor: Colors.white,
-        body: Column(children: [
-          NavBarTitleCl(
-            title: 'Sign In',
-            navWidget: BackIconWidget(width: width),
-            width: width,
-            height: height,
-          ),
-          SizedBox(height: height * 0.05),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * 0.1),
+        body: SafeArea(
+          child: BlocProvider(
+            create: (context) => AuthenticationBloc(
+                authenticationService: AuthenticationService()),
             child: Column(children: [
-              Form(
-                key: _formKey,
-                child: Column(
-                  children: [
-                    FormInputField(
-                      labelText: 'Email',
-                      controller: _emailController,
-                      keyboardType: TextInputType.emailAddress,
-                      height: height,
-                      obscureText: false,
-                      validator: validateEmail,
+              NavBarTitleCl(
+                title: 'Sign In',
+                navWidget: BackIconWidget(width: width),
+                width: width,
+                height: height,
+              ),
+              SizedBox(height: height * 0.05),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: width * 0.1),
+                child: Column(children: [
+                  Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        FormInputField(
+                          labelText: 'Email',
+                          controller: _emailController,
+                          keyboardType: TextInputType.emailAddress,
+                          height: height,
+                          obscureText: false,
+                          validator: validateEmail,
+                        ),
+                        SizedBox(height: height * 0.01),
+                        FormInputField(
+                          labelText: 'Password',
+                          controller: _passwordController,
+                          keyboardType: TextInputType.text,
+                          height: height,
+                          obscureText: true,
+                          validator: validatePassword,
+                        ),
+                      ],
                     ),
-                    SizedBox(height: height * 0.01),
-                    FormInputField(
-                      labelText: 'Password',
-                      controller: _passwordController,
-                      keyboardType: TextInputType.text,
-                      height: height,
-                      obscureText: true,
-                      validator: validatePassword,
-                    ),
-                  ],
+                  ),
+                  SizedBox(height: height * 0.05),
+                  BlocConsumer<AuthenticationBloc, AuthenticationState>(
+                    listener: (context, state) async {
+                      if (state is AuthenticationSuccess) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                              content: Text('Authentication successful'),
+                              backgroundColor: Colors.green),
+                        );
+                        userRole = (await getRole())!;
+                        if (userRole == 'user') {
+                          if (isFromFavorites) {
+                            Navigator.of(context).pop();
+                          } else {
+                            Navigator.popAndPushNamed(context, '/user_profile');
+                          }
+                        } else {
+                          Navigator.popAndPushNamed(context, '/admin_profile');
+                        }
+                      } else if (state is AuthenticationFailure) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text('${state.error}'),
+                            backgroundColor: Colors.redAccent,
+                          ),
+                        );
+                      }
+                    },
+                    builder: (context, state) {
+                      if (state is AuthenticationLoading) {
+                        return CircularProgressIndicator(
+                            color: Color(0xFFFF6E41));
+                      }
+                      return SubmitButton(
+                        text: 'Sign In',
+                        height: height * 0.06,
+                        width: width * 0.76,
+                        color: Color(0xFFFF6E41),
+                        textColor: Colors.white,
+                        onPressed: () => signIn(context),
+                      );
+                    },
+                  ),
+                  SizedBox(height: height * 0.05),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Not registered yet?',
+                        style: TextStyle(
+                          color: Color(0xFF808080),
+                          fontFamily: 'Poppins',
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, '/sign_up');
+                          AppMetrica.reportEvent(
+                              'ButtonSignUpFromSignIn Clicked');
+                        },
+                        child: Text(
+                          ' Sign up',
+                          style: TextStyle(
+                            color: Color(0xFFFF6E41),
+                            fontFamily: 'Poppins',
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: height * 0.02),
+                ]),
+              ),
+              Spacer(),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: SubmitButton(
+                  text: 'Skip',
+                  onPressed: () {
+                    userRole = '';
+                    Navigator.pushNamed(context, '/home');
+                  },
+                  height: height * 0.06,
+                  width: width * 0.76,
+                  color: Color(0xFFFFE0D7),
+                  textColor: Color(0xFFFF6E41),
+                  path: '/home',
                 ),
               ),
               SizedBox(height: height * 0.05),
-              BlocConsumer<AuthenticationBloc, AuthenticationState>(
-                listener: (context, state) async {
-                  if (state is AuthenticationSuccess) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text('Authentication successful'),
-                      backgroundColor: Colors.green),
-                    );
-                    userRole = (await getRole())!;
-                    if (userRole == 'user') {
-                      if (isFromFavorites) {
-                        Navigator.of(context).pop();
-                      } else {
-                        Navigator.popAndPushNamed(context, '/user_profile');
-                      }
-                    } else {
-                      Navigator.popAndPushNamed(context, '/admin_profile');
-                    }
-                  } else if (state is AuthenticationFailure) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text('${state.error}'),
-                        backgroundColor: Colors.redAccent,
-                      ),
-                    );
-                  }
-                },
-                builder: (context, state) {
-                  if (state is AuthenticationLoading) {
-                    return CircularProgressIndicator(color: Color(0xFFFF6E41));
-                  }
-                  return SubmitButton(
-                    text: 'Sign In',
-                    height: height * 0.06,
-                    width: width * 0.76,
-                    color: Color(0xFFFF6E41),
-                    textColor: Colors.white,
-                    onPressed: () => signIn(context),
-                  );
-                },
-              ),
-              SizedBox(height: height * 0.05),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Not registered yet?',
-                    style: TextStyle(
-                      color: Color(0xFF808080),
-                      fontFamily: 'Poppins',
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pushNamed(context, '/sign_up');
-                      AppMetrica.reportEvent('ButtonSignUpFromSignIn Clicked');
-                    },
-                    child: Text(
-                      ' Sign up',
-                      style: TextStyle(
-                        color: Color(0xFFFF6E41),
-                        fontFamily: 'Poppins',
-                        fontSize: 16,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: height * 0.02),
             ]),
           ),
-          Spacer(),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: SubmitButton(
-              text: 'Skip',
-              onPressed: () {
-                userRole = '';
-                Navigator.pushNamed(context, '/home');
-              },
-              height: height * 0.06,
-              width: width * 0.76,
-              color: Color(0xFFFFE0D7),
-              textColor: Color(0xFFFF6E41),
-              path: '/home',
-            ),
-          ),
-          SizedBox(height: height * 0.05),
-        ]),
-      ),
-    ));
+        ));
   }
 
   Future<String?> getRole() async {
